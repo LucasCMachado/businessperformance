cd157752fe0fa4d5d131a9fda6a44f1cf0217f56
Lucas César <lucas.c.machado@tr.com>
2019-06-10 11:50:08 -0300

    dompdf

---
+++ dompdf/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,135 @@
+<?php return array (
+  'sans-serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Helvetica',
+    'bold' => DOMPDF_FONT_DIR . 'Helvetica-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Helvetica-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Helvetica-BoldOblique',
+  ),
+  'times' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'times-roman' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'courier' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'helvetica' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Helvetica',
+    'bold' => DOMPDF_FONT_DIR . 'Helvetica-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Helvetica-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+    'bold_italic' => DOMPDF_FONT_DIR . 'ZapfDingbats',
+  ),
+  'symbol' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold' => DOMPDF_FONT_DIR . 'Symbol',
+    'italic' => DOMPDF_FONT_DIR . 'Symbol',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Symbol',
+  ),
+  'serif' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Times-Roman',
+    'bold' => DOMPDF_FONT_DIR . 'Times-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Times-Italic',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Times-BoldItalic',
+  ),
+  'monospace' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'fixed' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'Courier',
+    'bold' => DOMPDF_FONT_DIR . 'Courier-Bold',
+    'italic' => DOMPDF_FONT_DIR . 'Courier-Oblique',
+    'bold_italic' => DOMPDF_FONT_DIR . 'Courier-BoldOblique',
+  ),
+  'dejavu sans' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSans-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSans-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSans-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans',
+  ),
+  'dejavu sans light' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSans-ExtraLight',
+  ),
+  'dejavu sans condensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansCondensed',
+  ),
+  'dejavu sans mono' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-BoldOblique',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSansMono-Oblique',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSansMono',
+  ),
+  'dejavu serif' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerif-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerif-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerif',
+  ),
+  'dejavu serif condensed' => 
+  array (
+    'bold' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Bold',
+    'bold_italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-BoldItalic',
+    'italic' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed-Italic',
+    'normal' => DOMPDF_FONT_DIR . 'DejaVuSerifCondensed',
+  ),
+  'glyphicons halflings' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . '61a35f9d0a85d42e910ec3b0e6136e1e',
+  ),
+  'fontawesome' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'e0f829b7ded0578607fcbfeb8a68a5b0',
+  ),
+  'ionicons' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'fbaa0b0715a52191b5a40e7d71cddbfb',
+  ),
+  'fontgotham' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'd5ee655581212da539e2bead61932e7c',
+  ),
+  'gotham-ultra' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'd5ee655581212da539e2bead61932e7c',
+  ),
+  'gotham-medium' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . '1cda9683e511d33151267694e5aa6d00',
+  ),
+) ?>
