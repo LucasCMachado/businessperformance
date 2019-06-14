4d0203a3fcf4177f75b50ed2f41808dc5938b1b6
Machado <Lucas.C.Machado@thomsonreuters.com>
2019-06-14 14:05:09 -0300

    ajustes

--- cliente 2/head_main.php
+++ cliente 2/head_main.php
@@ -5,24 +5,24 @@
 <!-- ================== GOOGLE FONTS ==================-->
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500" rel="stylesheet">
 <!-- ======================= GLOBAL VENDOR STYLES ========================-->
-<link rel="stylesheet" href="../assets/css/vendor/bootstrap.css">
-<link rel="stylesheet" href="../assets/vendor/metismenu/dist/metisMenu.css">
-<link rel="stylesheet" href="../assets/vendor/switchery-npm/index.css">
-<link rel="stylesheet" href="../assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
+<link rel="stylesheet" href="assets/css/vendor/bootstrap.css">
+<link rel="stylesheet" href="assets/vendor/metismenu/dist/metisMenu.css">
+<link rel="stylesheet" href="assets/vendor/switchery-npm/index.css">
+<link rel="stylesheet" href="assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
 <!-- ======================= LINE AWESOME ICONS ===========================-->
-<link rel="stylesheet" href="../assets/css/icons/line-awesome.min.css">
-<link rel="stylesheet" href="../assets/css/icons/simple-line-icons.css">
+<link rel="stylesheet" href="assets/css/icons/line-awesome.min.css">
+<link rel="stylesheet" href="assets/css/icons/simple-line-icons.css">
 <!-- ======================= DRIP ICONS ===================================-->
-<link rel="stylesheet" href="../assets/css/icons/dripicons.min.css">
+<link rel="stylesheet" href="assets/css/icons/dripicons.min.css">
 <!-- ======================= MATERIAL DESIGN ICONIC FONTS =================-->
-<link rel="stylesheet" href="../assets/css/icons/material-design-iconic-font.min.css">
+<link rel="stylesheet" href="assets/css/icons/material-design-iconic-font.min.css">
 <!-- ======================= PAGE VENDOR STYLES ===========================-->
-<link rel="stylesheet" href="../assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.css">
+<link rel="stylesheet" href="assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.css">
 <!-- ======================= GLOBAL COMMON STYLES ============================-->
-<link rel="stylesheet" href="../assets/css/common/main.bundle.css">
+<link rel="stylesheet" href="assets/css/common/main.bundle.css">
 <!-- ======================= LAYOUT STYLES ===========================-->
-<link rel="stylesheet" href="../assets/css/layouts/horizontal/core/main.css">
+<link rel="stylesheet" href="assets/css/layouts/horizontal/core/main.css">
 <!-- ======================= MENU TYPE ===========================-->
-<link rel="stylesheet" href="../assets/css/layouts/horizontal/menu-type/default.css">
+<link rel="stylesheet" href="assets/css/layouts/horizontal/menu-type/default.css">
 <!-- ======================= THEME COLOR STYLES ===========================-->
-<link rel="stylesheet" href="../assets/css/layouts/horizontal/themes/theme-a.css">
+<link rel="stylesheet" href="assets/css/layouts/horizontal/themes/theme-a.css">
